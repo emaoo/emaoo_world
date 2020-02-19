d6714b2db22b1d6db4528d34d8cbe52c1ebe41a6
Matt <mjkern@andrew.cmu.edu>
2020-02-19 15:21:28 -0500

    added article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,10 @@ class Article < ApplicationRecord
   # relationships
   belongs_to :category
 
+  # validations
+  validates_presence_of :title
+  validates_presence_of :content
+
   # scopes
   scope :active, -> { where(active: true) }
   scope :alphabetical, -> { order('title') }
